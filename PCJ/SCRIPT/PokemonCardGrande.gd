d445d96c8410e9a3f4702e2f00a709c9d2722848
DarkNeku <darksoul.kun@gmail.com>
2025-11-21 01:56:26 -0300

    21/11 1:56

--- PCJ/SCRIPT/PokemonCardGrande.gd
+++ PCJ/SCRIPT/PokemonCardGrande.gd
@@ -5,6 +5,10 @@ extends VBoxContainer
 @onready var exp_actual_label = $HBoxContainer/Panel/EXP_ACT # Label para experiencia actual
 @onready var ps_line_edit = $HBoxContainer/Panel/PS_LINE # LineEdit para PS actual
 @onready var exp_line_edit = $HBoxContainer/Panel/EXP_LINE # LineEdit para experiencia actual
+@onready var btn_mas_ps = $HBoxContainer/Panel/MAS_PS
+@onready var btn_menos_ps = $HBoxContainer/Panel/MENOS_PS
+@onready var btn_mas_exp = $HBoxContainer/Panel/MAS_EXP
+@onready var btn_menos_exp = $HBoxContainer/Panel/MENOS_EXP
 
 var id_pokemon = ""
 
@@ -28,6 +32,41 @@ func configurar(ruta_imagen: String, ps_max: int, exp_actual: int, ps_actual: in
 		ps_line_edit.text = str(ps_actual if ps_actual != -1 else ps_max)
 	if exp_line_edit:
 		exp_line_edit.text = str(exp_actual)
+
+func _ready():
+	if btn_mas_ps:
+		btn_mas_ps.pressed.connect(_on_mas_ps_pressed)
+	if btn_menos_ps:
+		btn_menos_ps.pressed.connect(_on_menos_ps_pressed)
+	if btn_mas_exp:
+		btn_mas_exp.pressed.connect(_on_mas_exp_pressed)
+	if btn_menos_exp:
+		btn_menos_exp.pressed.connect(_on_menos_exp_pressed)
+
+func _on_mas_ps_pressed():
+	if ps_line_edit and ps_max_label:
+		var valor_actual = int(ps_line_edit.text)
+		var valor_max = int(ps_max_label.text)
+		if valor_actual < valor_max:
+			ps_line_edit.text = str(valor_actual + 1)
+
+func _on_menos_ps_pressed():
+	if ps_line_edit:
+		var valor_actual = int(ps_line_edit.text)
+		if valor_actual > 0:
+			ps_line_edit.text = str(valor_actual - 1)
+
+func _on_mas_exp_pressed():
+	if exp_line_edit:
+		var valor_actual = int(exp_line_edit.text)
+		if valor_actual < 999:
+			exp_line_edit.text = str(valor_actual + 1)
+
+func _on_menos_exp_pressed():
+	if exp_line_edit:
+		var valor_actual = int(exp_line_edit.text)
+		if valor_actual > 0:
+			exp_line_edit.text = str(valor_actual - 1)
 
 func get_id():
 	return id_pokemon
